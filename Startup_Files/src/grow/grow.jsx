abf1387a760d4f3f15230ee18bf3fed5a9c852c7
ClayDunford <dunfordclay@gmail.com>
2025-10-06 18:07:57 -0600

    added grow component

--- Startup_Files/src/grow/grow.jsx
+++ Startup_Files/src/grow/grow.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import '../app.css';
 
 export function Grow() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>grow displayed here</div>
-    </main>
+    <div className="d-flex flex-column bg-custom min-vh-100">
+      <main>
+        <div className="text-light text-center" style={{ backgroundColor: '#006838'}} >
+          <h1>Placeholder for Web App (will incorporate 3rd party api into the app)</h1>
+        </div>
+        <img src="/WindowTest.svg" alt="Temp image for the web application" />
+      </main>
+    </div>
   );
 }
