64c5ce44f7c7c54364544796064a91cb69499193
ClayDunford <dunfordclay@gmail.com>
2025-10-22 16:42:08 -0600

    modified grow page and added the jsx and js file structure

--- Startup_Files/src/grow/grow.jsx
+++ Startup_Files/src/grow/grow.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
 import '../app.css';
+import Succulent from './succulent';
+import Controls from './controls';
+import {useGrowthSimulation} from './hooks/useGrowthSimulation';
 
 export function Grow() {
+  const [potColor, setPotColor] = useState('#a97c50');
+  const [water, setWater] = useState(6);
+
+  const size = useGrowthSimulation({water, tickRate: 1000});
+
+  const {weather, error} = useWeather();
+  const background = getWeatherBackground(weather?.weathercode);
   return (
     <div className="d-flex flex-column bg-custom min-vh-100">
       <main>
         <div className="text-light text-center" style={{ backgroundColor: '#006838'}} >
-          <h1>Placeholder for Web App (will incorporate 3rd party api into the app)</h1>
+          <h1>Enjoy the growth!</h1>
+          {error? <p>{error}</p> : <p>Weather-based background active</p>}
         </div>
-        <img src="/WindowTest.svg" alt="Temp image for the web application" />
+        <div className="d-flex flex-column align-items-center mt-4">
+          <Succulent size={size} potColor={potColor} />
+          <Controls
+            potColor={potColor}
+            setPotColor={setPotColor}
+            water={water}
+            setWater={setWater} />
+            <p className="text-light mt-3"> Current Size: {size.toFixed(2)}x</p>
+        </div>
       </main>
     </div>
   );
---
+++ Startup_Files/src/grow/succulent.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default function Succulent({ size, potColor }) {
+    return (
+        <div
+            style={{
+                transform: `scale(${size})`,
+                transition: 'transform 1s ease',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+            }}
+        >
+             <img src="/WindowTest.svg" alt="Succulent" width="200" />
+        <div
+            style={{
+                width: '120px',
+                height: '40px',
+                backgroundColor: potColor,
+                borderTopLeftRadius: '12px',
+                borderTopRightRadius: '12px',
+                marginTop: '-10px',
+                boxShadow: '0 4px 10px rgba(0, 0, 0, .3)',
+            }}
+            />
+        </div>
+    )   
+}
